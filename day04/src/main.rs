d7e54c7f56a7a69e8ec4c0f7513d6bd2f495b8d0
Mathijs van der Vlies <mathijsvdvlies@solcon.nl>
2024-12-12 18:07:09 +0100

    Wrong implementation, but with good debugging

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,11 +1,15 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+use regex::Regex;
 
 fn main() {
     let word_search = load_word_search();
-    let directions = get_directions(&word_search);
-    for direction in directions {
-        println!("{}", direction);
+    let n_matches_per_direction = get_n_matches_per_direction(&word_search, "XMAS");
+
+    // Print the number of matches per direction
+    for (direction, n_matches) in n_matches_per_direction.iter() {
+        println!("{}: {}", direction, n_matches);
     }
 }
 
@@ -25,6 +29,48 @@ fn load_word_search() -> Vec<Vec<char>> {
 
     word_search
 }
+
+
+fn get_n_matches_per_direction(word_search: &Vec<Vec<char>>, word: &str) -> HashMap<String, usize> {
+    let directions = get_directions(word_search);
+    let mut n_matches_per_direction: HashMap<String, usize> = HashMap::new();
+
+    for direction in directions.iter() {
+        n_matches_per_direction.insert(direction.to_string(), _get_n_matches_in_direction(direction, word));
+    }
+
+    n_matches_per_direction
+}
+
+
+fn _get_n_matches_in_direction(direction: &str, word: &str) -> usize {
+    let mut n_matches = 0;
+    let chars: Vec<String> = word.chars().map(|c| c.to_string()).collect();
+    let regex_string = chars.join(".*");
+    let regex = Regex::new(&regex_string).unwrap();
+
+    for _ in regex.captures_iter(&direction) {
+        n_matches += 1;
+    }
+
+    for _ in regex.captures_iter(&reverse_direction(&direction)) {
+        n_matches += 1;
+    }
+
+    n_matches
+}
+
+
+fn reverse_direction(directions: &str) -> String {
+    let mut reversed = String::new();
+
+    for c in directions.chars().rev() {
+        reversed.push(c);
+    }
+
+    reversed
+}
+
 
 fn get_directions(word_search: &Vec<Vec<char>>) -> Vec<String> {
     let rows = get_rows(word_search);
